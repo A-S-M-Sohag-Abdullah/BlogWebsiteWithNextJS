0caeae34ca210b689647c36a6ac9aa8e8d747c9f
A-S-M-Sohag-Abdullah <sohagadullah2021@gmail.com>
2025-06-30 00:23:18 +0600

    Admin panel nav bar

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { useRouter } from "next/navigation";
 import { RootState, AppDispatch } from "@/store";
 import { logoutAdmin, validateAdminSession } from "@/store/slices/adminSlice";
+import Link from "next/link";
 
 export default function AdminLayout({
   children,
@@ -29,17 +30,37 @@ export default function AdminLayout({
     if (!loading && !isLoggedIn) {
       router.push("/secret-admin-login");
     }
-  }, [loading, isLoggedIn,router]);
+  }, [loading, isLoggedIn, router]);
 
   if (loading) return <div className="p-6 text-center">Checking auth...</div>;
   return (
     <>
-      <button
-        onClick={handleLogout}
-        className="text-white float-end me-5 px-3 py-2 rounded bg-red-500"
-      >
-        Logout
-      </button>
+      <div className="mb-3 flex justify-end">
+        <Link
+          href="/admin/createblog"
+          className="text-white  me-5 px-3 py-2 rounded bg-blue-500"
+        >
+          Create Blog
+        </Link>
+        <Link
+          href="/admin/categories/create"
+          className="text-white  me-5 px-3 py-2 rounded bg-blue-500"
+        >
+          Create Category
+        </Link>
+        <Link
+          href="/admin/blogs"
+          className="text-white  me-5 px-3 py-2 rounded bg-blue-500"
+        >
+          Manage Blogs
+        </Link>
+        <button
+          onClick={handleLogout}
+          className="text-white  me-5 px-3 py-2 rounded bg-red-500"
+        >
+          Logout
+        </button>
+      </div>
       {children}{" "}
     </>
   );
